cdf00b5f2d3c9de8be070c781ce1c54b86a20624
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-01-02 17:51:40 +0900

    Hello Java
    Second Day in Java.

---
+++ CommentJava.java
@@ -0,0 +1,17 @@
+public class CommentJava {
+    // psvm = public static void main(String[] args) {}
+    // sout = System.out.pringln();
+
+    // 한 줄 주석(single line comment)
+    /*
+        여러 줄 주석(multi line comment)
+    */
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1을 출력합니다.
+        System.out.println("hello java2");
+        /*
+        System.out.println("hello java3"); // hello
+        System.out.println("hello java4"); // hello
+         */
+    }
+}
