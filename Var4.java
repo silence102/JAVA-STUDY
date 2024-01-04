8ec938e09ae66498f76e79059d84cf440270040f
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-01-04 11:23:17 +0900

    Variable declaration, initialization

---
+++ Var4.java
@@ -0,0 +1,16 @@
+package variable;
+/*
+    변수 선언:
+    컴퓨터 메모리 공간을 확보해서 그곳에 데이터를 저장
+    변수 이름을 통해서 해당 메모리 공간에 접근
+    -> 데이터를 보관할 수 있는 공간을 만들고 그곳에 이름을 부여하는 과정
+ */
+public class Var4 {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        int c,d; // 여러 변수 한번에 선언 가능
+    }
+}
