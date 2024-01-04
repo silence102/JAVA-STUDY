5bf04b98ce52e3dd3c919e120e403235cef3daa0
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-01-05 00:48:02 +0900

    variable examples

---
+++ VarEx3.java
@@ -0,0 +1,13 @@
+package variable.ex;
+// Tip. soutv 입력 시 가까운 변수를 가져와 값과 함께 출력해준다.
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
