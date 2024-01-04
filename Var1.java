625e68952a461d2eaf754d473825ede142e37279
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-01-04 11:21:46 +0900

    start variable.

---
+++ Var1.java
@@ -0,0 +1,15 @@
+package variable;
+// 변수는 이름 그대로 변할 수 있다는 뜻.
+/*
+    Variable(변수):
+    수작업으로 변경하는 과정을 간편하게 하기 위함
+    값을 보관해두고 필요할 때 값을 꺼내서 읽을 수 있는 저장소 역할
+*/
+public class Var1 {
+
+    public static void main(String[] args) {
+        System.out.println(20); // 10 -> 20
+        System.out.println(20); // 10 -> 20
+        System.out.println(20); // 10 -> 20
+    }
+}
---
+++ Var2.java
@@ -0,0 +1,16 @@
+package variable;
+// int: 숫자 정수(integer)
+public class Var2 {
+
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 20; // 변수 초기화
+        /*
+        =: 오른쪽에 있는 값을 왼쪽에 저장한다는 뜻.
+        (수학에서 뜻하는 값이 같다(equals)와는 다른 뜻이다.)
+        */
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
