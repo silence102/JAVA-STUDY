f630cd3f5386b214e46b7d31f1b4888a394de530
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-01-07 21:01:22 +0900

    operator precedence (연산자 우선순위)

---
+++ Operater3.java
@@ -0,0 +1,12 @@
+package operater;
+// 연산자 우선순위
+
+public class Operater3 {
+
+    public static void main(String[] args) {
+        int sum1 = 1 + 2 * 3; // 1 + (2 * 3)
+        int sum2 = (1 + 2) * 3;
+        System.out.println("sum1 = " + sum1);
+        System.out.println("sum2 = " + sum2);
+    }
+}
