516e5f59bf7ed60b6f7f5d7056a1c9c3bc10db90
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-01-19 23:03:32 +0900

    5. while 반복문(loop) 1

---
+++ While1_1.java
@@ -0,0 +1,19 @@
+package loop;
+// 반복문이 필요한 코드 예시
+public class While1_1 {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        count = count + 1;
+        System.out.println("현재 숫자는:" + count);
+        count = count + 1;
+        System.out.println("현재 숫자는:" + count);
+        count = count + 1;
+        System.out.println("현재 숫자는:" + count);
+        count = count + 1;
+        System.out.println("현재 숫자는:" + count);
+        count = count + 1;
+        System.out.println("현재 숫자는:" + count);
+    }
+}
---
+++ While1_2.java
@@ -0,0 +1,13 @@
+package loop;
+// while 반복문을 활용한 코드 예시
+public class While1_2 {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 5) {
+            count = count + 1;
+            System.out.println("현재 숫자는 : " + count);
+        }
+    }
+}
